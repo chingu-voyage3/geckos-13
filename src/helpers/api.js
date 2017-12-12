766228685243219ba6346953f439116e330fd69d
Herdis Maria <herdismaria@herdiss-macbook-pro.home>
2017-12-12 15:53:24 +0100

    still trying to communicate with api

--- src/helpers/api.js
+++ src/helpers/api.js
@@ -1,15 +1,12 @@
-import axios from 'axios';
 import { cocktailThumbnailFactory, queryFactory } from '../helpers/helpers';
 import { data } from '../helpers/data';
 import { api } from './apiConfig';
 
 export const fetchCocktails = ingredients => {
-  axios
-    .get(
-      'http://addb.absolutdrinks.com/drinks/with/apple?apiKey=cdcb71827e3940e19651adcf0426058a'
-    )
-    .then(res => {
+  fetch('http://addb.absolutdrinks.com/drinks/with/apple/?appId=15937').then(
+    res => {
       console.log(res);
       return res;
-    });
+    }
+  );
 };
